3402c07f14d5bb908f20b0bf990da1a3afdac2a4
seonjoo <seonjoo2030@hanmail.net>
2023-01-23 15:42:36 +0900

    react state 적용. 유데미에서 강의 듣고 정리 필요

--- src/App.js
+++ src/App.js
@@ -10,7 +10,22 @@ export default class App extends Component { // App 클래스에서 React 에서
     cursor: "pointer",
     float: "right"
   }
-  
+
+  state = {
+    todoData : [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false
+      }
+    ]
+  }
+
   // 동적으로 변화해야 하는 부분은 함수로 만든다.
   getStyle = () => {
     return (
@@ -21,23 +36,10 @@ export default class App extends Component { // App 클래스에서 React 에서
       }
     )
   } 
-
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false
-    }
-  ]
  
   handleClick = (id) => {
-      let newTodoData = this.todoData.filter(data => data.id != id);
-      console.log('newTodoData', newTodoData);
+      let newTodoData = this.state.todoData.filter(data => data.id != id);
+      this.setState({todoData : newTodoData});
   }
 
   render() { // component 클래스 안에 있는 render 함수. 함수형 프로그래밍에서는 render() 함수 사용하지 않아도 된다.
@@ -47,7 +49,7 @@ export default class App extends Component { // App 클래스에서 React 에서
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-          {this.todoData.map((data) => {
+          {this.state.todoData.map((data) => {
             return ( // key 속성이 있어야 warning이 발생하지 않는다.
               <div style={this.getStyle()} key={data.id}> 
                 <input type="checkbox" defaultChecked={false} />
