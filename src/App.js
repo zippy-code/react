53572fe8729a7f54b4563db3113e53e1609819d0
seonjoo <seonjoo2030@hanmail.net>
2023-01-23 16:17:41 +0900

    할 일 추가 버튼 생성 (전개 연산자 학습)

--- src/App.js
+++ src/App.js
@@ -13,18 +13,10 @@ export default class App extends Component { // App 클래스에서 React 에서
 
   state = {
     todoData : [
-      {
-        id: "1",
-        title: "공부하기",
-        completed: true
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        completed: false
-      }
-    ]
-  }
+    ],
+
+    value: "",
+  };
 
   // 동적으로 변화해야 하는 부분은 함수로 만든다.
   getStyle = () => {
@@ -42,6 +34,26 @@ export default class App extends Component { // App 클래스에서 React 에서
       this.setState({todoData : newTodoData});
   }
 
+  handleChange = (event) => {
+    this.setState({value: event.target.value})
+  }
+
+  handleSumit = (event) => {
+    // form 안에 input 을 전송할 때 페이지가 리로드 되는 것을 막음
+    event.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 더해주기 ...은 전개 연산자.
+    // 전개 연산자는 특정 객체 또는 배열의 값을 다른 객체, 배열로 복제하거나 옮길 때 사용한다.
+    this.setState({todoData: [...this.state.todoData, newTodo]});
+  } 
+
   render() { // component 클래스 안에 있는 render 함수. 함수형 프로그래밍에서는 render() 함수 사용하지 않아도 된다.
     return (// jsx 문법은 class="" 가 아니라 classname="" 이다.
       <div className="container">
@@ -58,6 +70,23 @@ export default class App extends Component { // App 클래스에서 React 에서
               </div>  
             )
             })}
+            <form style={{display: 'flex'}} onSubmit={this.handleSumit}>
+              <input 
+              type={'text'} 
+              name={"value"} 
+              style={{flex: '10', padding: '5px'}}
+              placeholder={"해야 할 일을 입력하세요."} 
+              value={this.state.value}
+              onChange={this.handleChange}>
+              </input>
+
+              <input 
+              type={'submit'} 
+              value={"입력"} 
+              className={'btn'} 
+              style={{flex: '1'}}>
+              </input>
+            </form>
         </div>
       </div>
     )
